eb0fa2ab4af86840aa7ba43b7d94ee3d992af181
Vikash Kumar <mrvikashkumar396@gmail.com>
2022-12-16 20:24:49 +0530

    Cleanup some code

--- js/script.js
+++ js/script.js
@@ -20,42 +20,41 @@ window.addEventListener('keydown', (e) => {
 function calcFunctionality(value) {
   let keyArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "Backspace", "+", "-", "*", "/", ".", "Delete", "Enter"];
   for(let key of keyArr){
-    
-  }
-  if (value == 0 || value == 1 || value == 2 || value == 3 || value == 4 || value == 5 || value == 6 || value == 7 || value == 8 || value == 9 || value == "Backspace" || value == "+" || value == "-" || value == "*" || value == "/" || value == "." || value == "Delete" || value == "Enter") {
-    if (value === "Backspace") {
-      tempArr.pop()
-      rstSc.value = tempArr.join('')
-    } else if (value == "Delete") {
-      rstSc.value = "";
-      tempArr = []
-    } else if (value == "Enter") {
-      if (rstSc.value.length > 0) {
-        let rstValue = eval(tempArr.join(""));
-        tempArr = [...String(rstValue)]
-        rstSc.value = rstValue
+    if (value == key) {
+      if (value === "Backspace") {
+        tempArr.pop()
+        rstSc.value = tempArr.join('')
+      } else if (value == "Delete") {
+        rstSc.value = "";
+        tempArr = []
+      } else if (value == "Enter") {
+        if (rstSc.value.length > 0) {
+          let rstValue = eval(tempArr.join(""));
+          tempArr = [...String(rstValue)]
+          rstSc.value = rstValue
+        }
+      } else if (value == "+" || value == "-" || value == "*" || value == "/") {
+        value = ` ${value} `;
+        tempArr.push(value)
+        rstSc.value = tempArr.join('');
+      } else {
+        tempArr.push(value)
+        rstSc.value = tempArr.join('');
       }
-    } else if (value == "+" || value == "-" || value == "*" || value == "/") {
-      value = ` ${value} `;
-      tempArr.push(value)
-      rstSc.value = tempArr.join('');
-    } else {
-      tempArr.push(value)
-      rstSc.value = tempArr.join('');
-    }
-    if (tempArr.length > 18) {
-      rstSc.classList.add("above-18-digit")
-    } else {
-      rstSc.classList.remove("above-18-digit")
-    }
-    if (tempArr.length > 27) {
-      rstSc.classList.add("above-27-digit")
-    } else {
-      rstSc.classList.remove("above-27-digit")
-    }
-    if (tempArr.length >= 40) {
-      alert("Enough")
-      return;
+      if (tempArr.length > 18) {
+        rstSc.classList.add("above-18-digit")
+      } else {
+        rstSc.classList.remove("above-18-digit")
+      }
+      if (tempArr.length > 27) {
+        rstSc.classList.add("above-27-digit")
+      } else {
+        rstSc.classList.remove("above-27-digit")
+      }
+      if (tempArr.length >= 40) {
+        alert("Enough")
+        return;
+      }
     }
   }
 }
