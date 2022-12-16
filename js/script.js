d7c74ffb95430872e5974aa0f723101822b6c25d
Vikash Kumar <mrvikashkumar396@gmail.com>
2022-12-16 12:56:01 +0530

    Cleanup javascript file

--- js/script.js
+++ js/script.js
@@ -4,71 +4,52 @@ let tempArr = []
 btns.forEach(element => {
   element.addEventListener('click', (e) => {
     let inputValue = e.currentTarget.dataset.btnValue;
-    if (inputValue === "Backspace") {
-      tempArr.pop()
-      rstSc.value = tempArr.join('')
-    } else if (inputValue == "Delete") {
-      rstSc.value = "";
-      tempArr = []
-    }else if(inputValue == "Enter"){
-      if(rstSc.value.length > 0){
-        let rstValue = eval(tempArr.join(""));
-        tempArr = [...String(rstValue)]
-        rstSc.value = rstValue
-      }
-    }else if(inputValue == "+" || inputValue == "-" || inputValue == "/"){
-      inputValue = ` ${inputValue} `;
-      tempArr.push(inputValue)
-      rstSc.value = tempArr.join('');
-    }else if(inputValue == "X"){
-      tempArr.push(" * ")
-      rstSc.value = tempArr.join('');
-    }else {
-      tempArr.push(inputValue)
-      rstSc.value = tempArr.join('');
-    }
+    calcFunctionality(inputValue);
   })
 })
 
 // addEventListener on window
-window.addEventListener('keydown', (e)=>{
+window.addEventListener('keydown', (e) => {
   let inputValue = e.key;
-  // console.log(e)
-  if(inputValue == 0 || inputValue == 1 || inputValue == 2 || inputValue == 3 || inputValue == 4 || inputValue == 5 || inputValue == 6 || inputValue == 7 || inputValue == 8 || inputValue == 9 || inputValue == "Backspace" || inputValue == "+" || inputValue == "-" || inputValue == "*" || inputValue == "/" || inputValue == "." || inputValue == "Delete" || inputValue == "Enter"){
-    if (inputValue === "Backspace") {
+  calcFunctionality(inputValue);
+})
+
+// calculator functionality
+function calcFunctionality(value) {
+  if (value == 0 || value == 1 || value == 2 || value == 3 || value == 4 || value == 5 || value == 6 || value == 7 || value == 8 || value == 9 || value == "Backspace" || value == "+" || value == "-" || value == "*" || value == "/" || value == "." || value == "Delete" || value == "Enter") {
+    if (value === "Backspace") {
       tempArr.pop()
       rstSc.value = tempArr.join('')
-    } else if (inputValue == "Delete") {
+    } else if (value == "Delete") {
       rstSc.value = "";
       tempArr = []
-    }else if(inputValue == "Enter"){
-      if(rstSc.value.length > 0){
+    } else if (value == "Enter") {
+      if (rstSc.value.length > 0) {
         let rstValue = eval(tempArr.join(""));
         tempArr = [...String(rstValue)]
         rstSc.value = rstValue
       }
-    }else if(inputValue == "+" || inputValue == "-" || inputValue == "*" || inputValue == "/"){
-      inputValue = ` ${inputValue} `;
-      tempArr.push(inputValue)
+    } else if (value == "+" || value == "-" || value == "*" || value == "/") {
+      value = ` ${value} `;
+      tempArr.push(value)
       rstSc.value = tempArr.join('');
-    }else {
-      tempArr.push(inputValue)
+    } else {
+      tempArr.push(value)
       rstSc.value = tempArr.join('');
     }
-    // console.log(tempArr.length)
-    if(tempArr.length > 18){
+    if (tempArr.length > 18) {
       rstSc.classList.add("above-18-digit")
-    }else{
+    } else {
       rstSc.classList.remove("above-18-digit")
     }
-    if(tempArr.length > 27){
+    if (tempArr.length > 27) {
       rstSc.classList.add("above-27-digit")
-    }else{
+    } else {
       rstSc.classList.remove("above-27-digit")
     }
-    if(tempArr.length >= 40){
+    if (tempArr.length >= 40) {
       alert("Enough")
       return;
     }
   }
-})
+}
